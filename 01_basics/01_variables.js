79cb2cbfe376679b71833c31786ffd84615b695e
MdAfrozAlam <68823153+MdAfrozAlam@users.noreply.github.com>
2023-08-23 13:00:07 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+
+const accountId=1234;
+
+let accountCity="Kol";
+
+var emailId= "alam@gmail.com";
+
+
+//accountId=123; not allowed
+
+accountCity="hyd";
+emailId="a@gmail.com";
+
+
+console.table([accountId, accountCity, emailId]);
